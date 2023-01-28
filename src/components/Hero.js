f9d0e2f3aac842ed4004269827e03735a8060fc5
Uche Stan <uchestan@192.168.1.107>
2023-01-28 12:25:25 +0300

    unwanted commit 2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -26,46 +26,55 @@ export default function Hero() {
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             </p>
 
-
-                    <div className="row ">
-
-                        <div className="col-6 hero-title ">
-                            <div className="hero-item">
-
-                                <h1>Little Lemon</h1>
-                                <div>
-                                    <h2 className="">Chicago</h2>
+                        </div>
 
 
-                                    <p>We are a family owned
-                                        Meaterranean restaurant.
-                                        focused on traditional recipes served with a modern twist.</p>
+                        <div className="row ">
+
+                            <div className="col-6 hero-title ">
+                                <div className="hero-item">
+
+                                    <h1>Little Lemon</h1>
+                                    <div>
+                                        <h2 className="">Chicago</h2>
+
+
+                                        <p>We are a family owned
+                                            Meaterranean restaurant.
+                                            focused on traditional recipes served with a modern twist.</p>
+                                    </div>
+
+                                    <a href="/#booking-page" className='mt-5 btn'>Reserve a Table</a>
+                                    <div className='col-6 col-sm-6 order-2 order-md-2 text-btn'>
+
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                        </p>
+
+
+                                        <div>
+                                            <button className=' btn btn-reserve-a-table'>
+                                                Reserve a Table
+                                            </button>
+                                        </div>
+
+
+                                    </div>
+
+
+
+                                    <div className="col-6  col-sm-7 order-3 order-md-3">
+
+                                        <img src={restaurantfood} alt="food service" />
+
+                                    </div>
+
                                 </div>
 
-                                <a href="/#booking-page" className='mt-5 btn'>Reserve a Table</a>
-                        <div className='col-6 col-sm-6 order-2 order-md-2 text-btn'>
-
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-
-
-                            <div>
-                                <button className=' btn btn-reserve-a-table'>
-                                    Reserve a Table
-                                </button>
                             </div>
 
-
                         </div>
 
-
-
-                        <div className="col-6  col-sm-7 order-3 order-md-3">
-
-                            <img src={restaurantfood} alt="food service" />
-
-                        </div>
 
                     </div>
 
